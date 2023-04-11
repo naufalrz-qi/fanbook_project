dec127ea981b2eaff45b05971c4e84d0ce7937e1
Naufal Rifqi Zuhrian <estarossa.nrz@gmail.com>
2023-04-11 21:42:40 +0800

    Flask Setup for Deployment 2

--- app.py
+++ app.py
@@ -1,47 +1,48 @@
-from flask import Flask, render_template, request, jsonify
-from pymongo import MongoClient
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
 # <!-- DATA DIRI -->
 # <!-- Nama               : Naufal Rifqi Zuhrian -->
 # <!-- ID                 : 4778007 -->
 # <!-- Universitas/Kampus : Universitas Bumigora Mataram -->
 # <!-- Kelompok/group     : 3 -->
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 MONGODB_URI = os.environ.get("MONGODB_URI")
 DB_NAME =  os.environ.get("DB_NAME")
 
-client = MongoClient(MONGODB_URI)
 
+client = MongoClient(MONGODB_URI)
 db = client[DB_NAME]
 
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
+@app.route('/')
 def home():
-    return render_template('index.html')
+   return render_template('index.html')
 
-@app.route('/info',methods=['GET'])
-def get_info():
-    my_name = request.args.get('my_name')
-    print(my_name)
-    return jsonify({
-        'msg' : 'GET info'
-    })
-    
-@app.route('/info',methods=['POST'])
-def post_info():
-    my_name = request.form.get('my_name')
-    print(my_name)
-    return jsonify({
-        'msg' : 'POST info'
-    })
+@app.route("/homework", methods=["POST"])
+def homework_post():
+    # sample_receive = request.form['sample_give']
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive,
+    }
+    db.fanmessages.insert_one(doc)
+    return jsonify({'msg':'POST request!'})
+
+@app.route("/homework", methods=["GET"])
+def homework_get():
+    message_list = list(db.fanmessages.find({}, {'_id': False}))
+    return jsonify({'messages': message_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=5000,debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
